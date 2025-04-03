ad7e8b5dd303eac2ef2dbaf79f22ac490191937a
Kevin Schneider <schneider.kev@outlook.de>
2025-04-03 15:08:23 +0200

    Testing - Create a unit test (Expecto)

--- tests/ExpectoTests/Tests.fs
+++ tests/ExpectoTests/Tests.fs
@@ -1,12 +1,19 @@
-module Tests
+namespace XUnitTests
 
-open System
 open Expecto
+open MyLibrary
 
-[<Tests>]
-let expectoTests = 
-    testList "ExpectoTests" [
-        test "Yes" {
-            Expect.isTrue true "No"
-        }
-    ]
+module MathFunctionsTests =
+
+    [<Tests>]
+    let expectoTests = 
+        testList "ExpectoTests" [
+            test "Adding 2 and 2 returns 4" {
+                let result = MathFunctions.add 2 2
+                Expect.equal result 4 "Result was incorrect"
+            }
+            test "Subtracting 2 from 4 returns 2" {
+                let result = MathFunctions.subtract 4 2
+                Expect.equal result 2 "Result was incorrect"
+            }
+        ]
