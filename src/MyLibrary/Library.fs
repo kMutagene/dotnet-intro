2e17d0b967b041c11c45a5ae580e468220cfd8f5
Kevin Schneider <schneider.kev@outlook.de>
2025-04-03 09:18:58 +0200

    Library - Write Some Library Code

--- src/MyLibrary/Library.fs
+++ src/MyLibrary/Library.fs
@@ -1,5 +1,9 @@
 ﻿namespace MyLibrary
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module MathFunctions =
+    let add x y = x + y
+    let subtract x y = x - y
+
+module StringUtils =
+    let toUpper (s: string) = s.ToUpper()
+    let toLower (s: string) = s.ToLower()
